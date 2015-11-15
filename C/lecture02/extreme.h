3f87246932fb7dce28ec765fd012c8480d668ec5
Dzejrou <dzejrou@gmail.com>
2015-11-15 19:41:12 +0100

    added documenting comments

--- C/lecture02/extreme.h
+++ C/lecture02/extreme.h
@@ -3,24 +3,42 @@
 
 #include <stdarg.h>
 
+/**
+ * Type representing a pointer to a function comparing two numbers.
+ */
 typedef int (*comparator)(int, int);
 
-typedef struct CMP_
+/**
+ * Struct representing an extreme selecting operation, holds
+ * a starting value of the extreme and a pointer to the comparing
+ * function.
+ */
+typedef struct cmp_t_
 {
 	int extreme;
 	comparator cmp_func;
 } cmp_t;
 
+/**
+ * Type representing a pointer to the extreme finding function,
+ * used for simplicity when loading this 
+ */
 typedef int (*ext_func)(cmp_t, int, ...);
 
-enum OP
+/**
+ * Enum representing possible extremes.
+ */
+enum op
 {
 	MIN = 0, MAX
 };
 
 /**
- * Loaded using dload().
- * int ext(cmp_t, int, ...);
-*/
+ * Brief: Returns an extreme from a given set of integers.
+ * Param: Struct containing the comparator function and the starting extreme value.
+ * Param: Number of the numbers being processed.
+ * Param: Variable argument list of numbers.
+ */
+//int ext(cmp_t, int, ...);
 
 #endif
